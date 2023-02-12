7996feb597932087cfe2acc1812ab9d4e89151f6
marcusziade <marcus.ziade@ouraring.com>
2023-02-12 11:10:12 +0200

    Add command arguments to define date range

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
@@ -14,37 +15,59 @@ type NasaAPOD struct {
 }
 
 func main() {
-	apods := getAPODsForLastWeek()
+	var start, end string
+	flag.StringVar(&start, "start", "", "start date (YYYY-MM-DD)")
+	flag.StringVar(&end, "end", "", "end date (YYYY-MM-DD)")
+	flag.Parse()
+
+	loadMsg := "Fetching APODs...\n\n"
+	fmt.Println(loadMsg)
+
+	apods, err := getAPODsForDateRange(start, end)
+	if err != nil {
+		fmt.Println("Error retrieving data:", err)
+		return
+	}
+
+	if len(apods) == 0 {
+		fmt.Println("No APODs found in the given date range")
+		return
+	}
 
 	for _, apod := range apods {
 		printAPOD(apod)
 	}
 }
 
-func getAPODsForLastWeek() []NasaAPOD {
-	endDate := time.Now()
-	startDate := endDate.AddDate(0, 0, -7)
-
-	url := fmt.Sprintf(
-		"https://api.nasa.gov/planetary/apod?api_key=efxF8oNY5ZPU48KF3waxgvnQnmITHxLknZpZz6Q8&start_date=%s&end_date=%s",
-		startDate.Format("2006-01-02"),
-		endDate.Format("2006-01-02"))
+func getAPODsForDateRange(start, end string) ([]NasaAPOD, error) {
+	var url string
+	if start == "" || end == "" {
+		endDate := time.Now()
+		startDate := endDate.AddDate(0, 0, -7)
+		url = fmt.Sprintf(
+			"https://api.nasa.gov/planetary/apod?api_key=efxF8oNY5ZPU48KF3waxgvnQnmITHxLknZpZz6Q8&start_date=%s&end_date=%s",
+			startDate.Format("2006-01-02"),
+			endDate.Format("2006-01-02"))
+	} else {
+		url = fmt.Sprintf(
+			"https://api.nasa.gov/planetary/apod?api_key=efxF8oNY5ZPU48KF3waxgvnQnmITHxLknZpZz6Q8&start_date=%s&end_date=%s",
+			start,
+			end)
+	}
 
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error retrieving data:", err)
-		return nil
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	var apods []NasaAPOD
 	err = json.NewDecoder(resp.Body).Decode(&apods)
 	if err != nil {
-		fmt.Println("Error decoding JSON data:", err)
-		return nil
+		return nil, err
 	}
 
-	return apods
+	return apods, nil
 }
 
 func printAPOD(apod NasaAPOD) {
