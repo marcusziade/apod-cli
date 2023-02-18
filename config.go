d6a6022ca3604fe6548fc01f5c2b399a33290913
marcusziade <marcus.ziade@ouraring.com>
2023-02-18 13:21:13 +0200

    Split config stuff into `config.go`

---
+++ config.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Config struct {
+	APIKey string
+}
+
+func GetOrCreateAPIKey() string {
+	filePathName := "Keys.json"
+	config, err := readConfig(filePathName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("API key not found in Keys.json")
+			fmt.Println("Please sign up for an API key at https://api.nasa.gov/#signUp")
+			fmt.Println("Once you have your API key, enter it below:")
+			reader := bufio.NewReader(os.Stdin)
+			apiKey, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatalf("Error reading API key input: %v", err)
+			}
+
+			apiKey = strings.TrimSpace(apiKey)
+
+			config := &Config{APIKey: apiKey}
+			err = writeConfig("Keys.json", config)
+			if err != nil {
+				log.Fatalf("Error saving API key to file: %v", err)
+			}
+
+			fmt.Printf("API key saved to %s.\n", filePathName)
+			return apiKey
+		}
+
+		log.Fatalf("Error reading Keys.json: %v", err)
+	}
+
+	apiKey := config.APIKey
+	return apiKey
+}
+
+// Reads the configuration data from a file.
+func readConfig(fileName string) (*Config, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	config := &Config{}
+	err = json.NewDecoder(file).Decode(config)
+	if err != nil {
+		fmt.Printf("Error decoding config file: %v\n", err)
+		return nil, err
+	}
+
+	return config, nil
+}
+
+// Saves the configuration data to a file.
+func writeConfig(fileName string, config *Config) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	err = json.NewEncoder(file).Encode(config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"strings"
 	"time"
 )
 
@@ -21,7 +17,7 @@ type NasaAPOD struct {
 const apiURL = "https://api.nasa.gov/planetary/apod"
 
 func main() {
-	apiKey := getOrCreateAPIKey()
+	apiKey := GetOrCreateAPIKey()
 	start, end := parseArgumentsForDateRange()
 
 	loadMsg := "Fetching APODs...\n\n"
@@ -96,75 +92,4 @@ func constructURL(apiKey, start, end string) string {
 		"%s?api_key=%s&start_date=%s&end_date=%s",
 		apiURL, apiKey,
 		start, end)
-}
-
-type Config struct {
-	APIKey string
-}
-
-func getOrCreateAPIKey() string {
-	filePathName := "Keys.json"
-	config, err := readConfig(filePathName)
-	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Println("API key not found in Keys.json")
-			fmt.Println("Please sign up for an API key at https://api.nasa.gov/#signUp")
-			fmt.Println("Once you have your API key, enter it below:")
-			reader := bufio.NewReader(os.Stdin)
-			apiKey, err := reader.ReadString('\n')
-			if err != nil {
-				log.Fatalf("Error reading API key input: %v", err)
-			}
-
-			apiKey = strings.TrimSpace(apiKey)
-
-			config := &Config{APIKey: apiKey}
-			err = writeConfig("Keys.json", config)
-			if err != nil {
-				log.Fatalf("Error saving API key to file: %v", err)
-			}
-
-			fmt.Printf("API key saved to %s.\n", filePathName)
-			return apiKey
-		}
-
-		log.Fatalf("Error reading Keys.json: %v", err)
-	}
-
-	apiKey := config.APIKey
-	return apiKey
-}
-
-// Reads the configuration data from a file.
-func readConfig(fileName string) (*Config, error) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	config := &Config{}
-	err = json.NewDecoder(file).Decode(config)
-	if err != nil {
-		fmt.Printf("Error decoding config file: %v\n", err)
-		return nil, err
-	}
-
-	return config, nil
-}
-
-// Saves the configuration data to a file.
-func writeConfig(fileName string, config *Config) error {
-	file, err := os.Create(fileName)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	err = json.NewEncoder(file).Encode(config)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
